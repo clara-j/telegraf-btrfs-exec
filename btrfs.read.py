371d97ac42a128436f3212095cfddd0add2f1c98
clara-j <jason@clarafamily.com>
2018-04-13 18:10:17 -0400

    Update to support df and usage

--- btrfs.read.py
+++ btrfs.read.py
@@ -14,10 +14,72 @@ def getPools(excludeList):
                pools.append(pool)
      return pools
 
-def getMeasurements(pool):
+def getFileSystemDFMeasurements(pool):
+    commandString = subprocess.check_output("sudo btrfs filesystem df -b " + pool, shell=True)
+
+    btrfsType = "df"
+
+    outArray = []
+
+    #split into section
+    commandArray = commandString.split('\n')
+
+
+    for i in range(len(commandArray)):
+        lineSections =  commandArray[i].replace(':',',').split(',')
+        if len(lineSections) > 1:
+            metric = lineSections[0].strip()
+            raidType = lineSections[1].strip()
+            total = lineSections[2].strip()
+            used = lineSections[3].strip()
+            free = str(long(total.split('=')[1]) - long(used.split('=')[1]))
+
+            outArray.append( "btrfs,command=" + btrfsType + ",type=" + metric + ",raidType="+ raidType +",pool=" + pool + " " + total)
+            outArray.append( "btrfs,command=" + btrfsType + ",type=" + metric + ",raidType="+ raidType +",pool=" + pool + " " + used)
+            outArray.append( "btrfs,command=" + btrfsType + ",type=" + metric + ",raidType=" + raidType + ",pool=" + pool + " free=" + free)
+
+    return outArray
+
+def getFileSystemUsageMeasurements(pool):
+    commandString = subprocess.check_output("sudo btrfs filesystem usage -b " + pool, shell=True)
+    btrfsType = "usage"
+    outArray = []
+
+    #split into section
+    sectionArray = commandString.split('\n\n')
+
+
+    for i in range(len(sectionArray)):
+        #split into rows
+        measurementLines  = sectionArray[i].split('\n')
+        if "Overall:" in measurementLines[0]:
+            for j in range(1, len(measurementLines)):
+                measurementLinesSection = measurementLines[j].split(':')
+                metric = measurementLinesSection[0].strip().replace(' ', '_')
+                value = measurementLinesSection[1].strip().split('\t')[0]
+                outArray.append("btrfs,command=" + btrfsType + ",type="+ metric +",pool="+ pool +" value="+ value)
+        else:
+            type = measurementLines[0].replace(':',',').split(',')[0]
+            for j in range(1, len(measurementLines)):
+                if len(measurementLines[j]) > 1:
+                    measurementLinesSection = measurementLines[j].strip().split('\t')
+                    drive = measurementLinesSection[0].strip()
+                    value = measurementLinesSection[1].strip()
+                    if DEBUG:
+                        print("Drive: " + drive)
+                        print("Type: " + type)
+                        print("Value: " + value)
+                    outArray.append("btrfs,command=" + btrfsType + ",type="+ type +",drive="+ drive +",pool="+ pool +" value="+ value)
+
+    return outArray
+
+
+def getDeviceStatMeasurements(pool):
     statString = subprocess.check_output("sudo btrfs device stat "+ pool, shell=True)
     statArray = statString.split('\n')
     outArray = []
+    btrfsType = "stat"
+
     for i in range(len(statArray)):
         a = statArray[i].split()
         if len(a) > 1:
@@ -29,7 +91,7 @@ def getMeasurements(pool):
                 print("Drive: "+ drive)
                 print("Metric: "+ metric)
                 print("Value: "+ value)
-             outArray.append("btrfs,type="+ metric +",drive="+ drive +",pool="+ pool +" value="+ value)
+             outArray.append("btrfs,command=" + btrfsType + ",type="+ metric +",drive="+ drive +",pool="+ pool +" value="+ value)
     return outArray
 
 
@@ -46,7 +108,14 @@ for i in range(len(pools)):
     if DEBUG:
         print "----------- Measurements -----------"
         print "Pool: "+ pools[i]
-    measurments = getMeasurements(pools[i])
-    for j in range(len(measurments)):
-        print measurments[j]
+    deviceStatMeasurements = getDeviceStatMeasurements(pools[i])
+    for j in range(len(deviceStatMeasurements)):
+        print deviceStatMeasurements[j]
 
+    filesystemDFMeasurements = getFileSystemDFMeasurements(pools[i])
+    for j in range(len(filesystemDFMeasurements)):
+        print filesystemDFMeasurements[j]
+
+    filesystemUsageMeasurements = getFileSystemUsageMeasurements(pools[i])
+    for j in range(len(filesystemUsageMeasurements)):
+        print filesystemUsageMeasurements[j]
